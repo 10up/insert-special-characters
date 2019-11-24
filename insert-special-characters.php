0cfeba034480ac4182829c472e259438bc494981
Jeffrey Paul <jeff.paul@10up.com>
2019-11-23 23:17:48 -0600

    update license in insert-special-characters.php to GPLv2

--- insert-special-characters.php
+++ insert-special-characters.php
@@ -8,8 +8,8 @@
  * Requires PHP:      5.6
  * Author:            10up
  * Author URI:        https://10up.com
- * License:           MIT
- * License URI:       https://www.gnu.org/licenses/mit.html
+ * License:           GPLv2
+ * License URI:       https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  * Text Domain:       insert-special-characters
  *
  * @package insert-special-characters
