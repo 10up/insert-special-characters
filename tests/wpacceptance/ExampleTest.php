ed72cc23d784ef23de19898b5061e78ce04837fa
John Watkins <johnwatkins0@gmail.com>
2020-04-06 21:21:51 -0500

    Set up WP Acceptance

---
+++ tests/wpacceptance/ExampleTest.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Example test class
+ *
+ * @package insert-special-characters
+ */
+
+/**
+ * PHPUnit test class
+ */
+class ExampleTest extends \TestCaseBase {
+
+	/**
+	 * Example test
+	 */
+	public function testExample() {
+		$this->assertTrue( true );
+	}
+}
---
+++ tests/wpacceptance/includes/TestCaseBase.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Test case class that provides us with some helper functionality.
+ *
+ * @package insert-special-characters
+ */
+
+/**
+ * Class extends \WPAcceptance\PHPUnit\TestCase
+ */
+class TestCaseBase extends \WPAcceptance\PHPUnit\TestCase {
+
+}
