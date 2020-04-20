019c70485e60f7446262345e963c0bb75bade1e1
John Watkins <johnwatkins0@gmail.com>
2020-04-19 20:51:14 -0500

    Convert TestCaseBase.php indentation to tabs

--- tests/wpacceptance/includes/TestCaseBase.php
+++ tests/wpacceptance/includes/TestCaseBase.php
@@ -9,14 +9,14 @@
  * Class extends \WPAcceptance\PHPUnit\TestCase
  */
 class TestCaseBase extends \WPAcceptance\PHPUnit\TestCase {
-    /**
+	/**
 	 * Dismiss the Gutenberg NUX tooltip.
 	 *
 	 * @param \WPAcceptance\PHPUnit\Actor $actor The actor.
 	 */
 	protected function dismissNUXTip( $actor ) {
-        if ( $actor->getElement( '.edit-post-welcome-guide' ) ) {
-            $actor->click( '.edit-post-welcome-guide .components-modal__header > button' );
-        }
+		if ( $actor->getElement( '.edit-post-welcome-guide' ) ) {
+			$actor->click( '.edit-post-welcome-guide .components-modal__header > button' );
+		}
 	}
 }
