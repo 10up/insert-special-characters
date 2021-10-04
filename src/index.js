75dfde9c41eddf307fe74af65ead1b28c939540b
Juan Pablo Amato <pabamato@gmail.com>
2021-10-04 21:48:16 +0200

    change format button placement

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const { registerFormatType, toggleFormat, insert } = wp.richText;
 const { Fragment } = wp.element;
-const { RichTextToolbarButton, RichTextShortcut } = wp.blockEditor;
-const { Popover } = wp.components;
+const { BlockControls, RichTextShortcut } = wp.blockEditor;
+const { Popover, ToolbarButton, ToolbarGroup } = wp.components;
 const { getRectangleFromRange } = wp.dom;
 const { applyFilters } = wp.hooks;
+const { displayShortcut } = wp.keycodes;
 const { __ } = wp.i18n;
 
 import { CharacterMap } from 'react-character-map';
@@ -43,89 +44,88 @@ registerFormatType( type, {
 				selection.rangeCount > 0 ? selection.getRangeAt( 0 ) : null;
 			onChange( toggleFormat( value, { type } ) );
 		};
-
 		// Pin the Popover to the caret position.
 		const anchorRect = () => {
 			return getRectangleFromRange( anchorRange );
 		};
-
+		const characters = applyFilters( `${ name }-characters`, Chars );
 		// Display the character map when it is active.
-		if ( isActive ) {
-			const characters = applyFilters( `${ name }-characters`, Chars );
-			return (
-				<Popover
-					className="character-map-popover"
-					position="bottom center"
-					focusOnMount="firstElement"
-					key="charmap-popover"
-					onClick={ () => {} }
-					getAnchorRect={ anchorRect }
-					expandOnMobile={ true }
-					headerTitle={ __(
-						'Insert Special Character',
+		const specialCharsPopover = isActive && (
+			<Popover
+				className="character-map-popover"
+				position="bottom center"
+				focusOnMount="container"
+				key="charmap-popover"
+				getAnchorRect={ anchorRect }
+				expandOnMobile={ true }
+				headerTitle={ __(
+					'Insert Special Character',
+					'insert-special-characters'
+				) }
+				onClose={ () => {
+					onChange( toggleFormat( value, { type } ) );
+				} }
+			>
+				<CharacterMap
+					characterData={ characters }
+					onSelect={
+						// Insert the selected character and close the popover.
+						( char ) => {
+							onChange( insert( value, char.char ) );
+						}
+					}
+					categoryNames={ {
+						Math: __( 'Math', 'insert-special-characters' ),
+						Currency: __( 'Currency', 'insert-special-characters' ),
+						Punctuation: __(
+							'Punctuation',
+							'insert-special-characters'
+						),
+						Misc: __( 'Misc', 'insert-special-characters' ),
+						Greek: __( 'Greek', 'insert-special-characters' ),
+						Latin: __( 'Latin', 'insert-special-characters' ),
+						Arrows: __( 'Arrows', 'insert-special-characters' ),
+					} }
+					categoriesLabelText={ __(
+						'Categories',
 						'insert-special-characters'
 					) }
-					onClose={ () => {
-						onChange( toggleFormat( value, { type } ) );
-					} }
-				>
-					<CharacterMap
-						characterData={ characters }
-						onSelect={
-							// Insert the selected character and close the popover.
-							( char ) => {
-								onChange( insert( value, char.char ) );
-							}
-						}
-						categoryNames={ {
-							Math: __( 'Math', 'insert-special-characters' ),
-							Currency: __(
-								'Currency',
-								'insert-special-characters'
-							),
-							Punctuation: __(
-								'Punctuation',
-								'insert-special-characters'
-							),
-							Misc: __( 'Misc', 'insert-special-characters' ),
-							Greek: __( 'Greek', 'insert-special-characters' ),
-							Latin: __( 'Latin', 'insert-special-characters' ),
-							Arrows: __( 'Arrows', 'insert-special-characters' ),
-						} }
-						categoriesLabelText={ __(
-							'Categories',
-							'insert-special-characters'
-						) }
-						characterListLabelText={ __(
-							'Character List',
-							'insert-special-characters'
-						) }
-						filterLabelText={ __(
-							'Filter',
-							'insert-special-characters'
-						) }
-						key="charmap"
-					/>
-				</Popover>
-			);
-		}
+					characterListLabelText={ __(
+						'Character List',
+						'insert-special-characters'
+					) }
+					filterLabelText={ __(
+						'Filter',
+						'insert-special-characters'
+					) }
+					key="charmap"
+				/>
+			</Popover>
+		);
 
 		return (
 			<Fragment>
-				<RichTextShortcut
-					type="primary"
-					character={ character }
-					onUse={ onToggle }
-				/>
-				<RichTextToolbarButton
-					className={ `toolbar-button-with-text toolbar-button__advanced-${ name }` }
-					icon="editor-customchar"
-					title={ title }
-					onClick={ onToggle }
-					isActive={ isActive }
-					shortcutType="primary"
-					shortcutCharacter={ character }
-				/>
+				<BlockControls group="inline">
+					<ToolbarGroup>
+						<ToolbarButton
+							className={ `toolbar-button-with-text toolbar-button__advanced-${ name }` }
+							icon="editor-customchar"
+							isPressed={ isActive }
+							label={ title }
+							onClick={ onToggle }
+							shortcut={ displayShortcut.primary( character ) }
+						/>
+					</ToolbarGroup>
+				</BlockControls>
+				<>
+					<RichTextShortcut
+						type="primary"
+						character={ character }
+						// eslint-disable-next-line no-console
+						onUse={ onToggle }
+					/>
+				</>
+				{ specialCharsPopover }
 			</Fragment>
 		);
 	},
