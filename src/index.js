459f6d79ea7d3f312d76f4be12c40800bf84b733
Ben Marshall <ben.s.marshall@icloud.com>
2023-06-08 15:28:51 -0500

    Fix for grid positioning when text is selected

--- src/index.js
+++ src/index.js
@@ -14,19 +14,20 @@ import Chars from '../node_modules/react-character-map/dist/component/chars.json
 
 const InsertSpecialCharactersOptions = {
 	name: 'insertspecialcharacters',
-	title: __( 'Special Characters', 'insert-special-characters' ),
+	title: __('Special Characters', 'insert-special-characters'),
 	character: 'o',
 	value: '',
 };
 
 const { name, title, character } = InsertSpecialCharactersOptions;
-const type = `special-characters/${ name }`;
+const type = `special-characters/${name}`;
 let anchorRange;
+let anchorRect;
 
 /**
  * Register the "Format Type" to create the character inserter.
  */
-registerFormatType( type, {
+registerFormatType(type, {
 	title,
 	tagName: name,
 	className: null,
@@ -40,42 +41,44 @@ registerFormatType( type, {
 	 * @param {boolean}  props.value    State of popover.
 	 * @param {Function} props.onChange Event handler to detect range selection.
 	 */
-	edit( { isActive, value, onChange } ) {
+	edit({ isActive, value, onChange }) {
 		const onToggle = () => {
 			// Set up the anchorRange when the Popover is opened.
 			const selection = document.defaultView.getSelection();
 
-			anchorRange =
-				selection.rangeCount > 0 ? selection.getRangeAt( 0 ) : null;
-			onChange( toggleFormat( value, { type } ) );
+			anchorRange = selection.rangeCount > 0 ? selection.getRangeAt(0) : null;
+
+			// Pin the Popover to the caret position.
+			let boundingClientRect = anchorRange
+				? anchorRange.getBoundingClientRect()
+				: null;
+			anchorRect = anchorRange ? () => boundingClientRect : null;
+
+			onChange(toggleFormat(value, { type }));
 		};
-		// Pin the Popover to the caret position.
-		const anchorRect = () => {
-			return anchorRange ? anchorRange.getBoundingClientRect() : null;
-		};
-		const characters = applyFilters( `${ name }-characters`, Chars );
+		const characters = applyFilters(`${name}-characters`, Chars);
 		// Display the character map when it is active.
 		const specialCharsPopover = isActive && (
 			<Popover
-				className="character-map-popover"
-				placement="bottom-start"
-				focusOnMount="firstElement"
-				key="charmap-popover"
-				getAnchorRect={ anchorRect }
-				expandOnMobile={ true }
-				headerTitle={ __(
+				className='character-map-popover'
+				placement='bottom-start'
+				focusOnMount='firstElement'
+				key='charmap-popover'
+				getAnchorRect={anchorRect}
+				expandOnMobile={true}
+				headerTitle={__(
 					'Insert Special Character',
 					'insert-special-characters'
-				) }
-				onClose={ () => {
-					onChange( toggleFormat( value, { type } ) );
-				} }
+				)}
+				onClose={() => {
+					onChange(toggleFormat(value, { type }));
+				}}
 			>
 				<CharacterMap
-					characterData={ characters }
+					characterData={characters}
 					onSelect={
 						// Insert the selected character and close the popover.
-						( char ) => {
+						(char) => {
 							const newValue = {
 								...value,
 								// duplicate the format at the value start to ensure the
@@ -83,72 +86,63 @@ registerFormatType( type, {
 								formats: value.formats.splice(
 									value.start,
 									0,
-									value.formats.at( value.start )
+									value.formats.at(value.start)
 								),
 								text: char.char,
 							};
 
-							onChange( insert( value, newValue ) );
+							onChange(insert(value, newValue));
 						}
 					}
-					categoryNames={ {
-						Math: __( 'Math', 'insert-special-characters' ),
-						Currency: __( 'Currency', 'insert-special-characters' ),
-						Punctuation: __(
-							'Punctuation',
-							'insert-special-characters'
-						),
-						Misc: __( 'Misc', 'insert-special-characters' ),
-						Greek: __( 'Greek', 'insert-special-characters' ),
-						Latin: __( 'Latin', 'insert-special-characters' ),
-						Arrows: __( 'Arrows', 'insert-special-characters' ),
-						Music: __( 'Music', 'insert-special-characters' ),
-					} }
-					categoriesLabelText={ __(
-						'Categories',
-						'insert-special-characters'
-					) }
-					characterListLabelText={ __(
+					categoryNames={{
+						Math: __('Math', 'insert-special-characters'),
+						Currency: __('Currency', 'insert-special-characters'),
+						Punctuation: __('Punctuation', 'insert-special-characters'),
+						Misc: __('Misc', 'insert-special-characters'),
+						Greek: __('Greek', 'insert-special-characters'),
+						Latin: __('Latin', 'insert-special-characters'),
+						Arrows: __('Arrows', 'insert-special-characters'),
+						Music: __('Music', 'insert-special-characters'),
+					}}
+					categoriesLabelText={__('Categories', 'insert-special-characters')}
+					characterListLabelText={__(
 						'Character List',
 						'insert-special-characters'
-					) }
-					filterLabelText={ __(
-						'Filter',
-						'insert-special-characters'
-					) }
-					key="charmap"
-					mostUsedPalette={ tenupIscVars.most_read_palette }
-					mostUsedPaletteLabelText={ __(
+					)}
+					filterLabelText={__('Filter', 'insert-special-characters')}
+					key='charmap'
+					mostUsedPalette={tenupIscVars.most_read_palette}
+					mostUsedPaletteLabelText={__(
 						'Most Used',
 						'insert-special-characters'
-					) }
+					)}
 				/>
 			</Popover>
 		);
 
 		return (
 			<Fragment>
-				<BlockControls group="other">
+				<BlockControls group='other'>
 					<ToolbarGroup>
 						<ToolbarButton
-							className={ `toolbar-button-with-text toolbar-button__advanced-${ name }` }
-							icon="editor-customchar"
-							isPressed={ isActive }
-							label={ title }
-							onClick={ onToggle }
-							shortcut={ displayShortcut.primary( character ) }
+							className={`toolbar-button-with-text toolbar-button__advanced-${name}`}
+							icon='editor-customchar'
+							isPressed={isActive}
+							label={title}
+							onClick={onToggle}
+							shortcut={displayShortcut.primary(character)}
 						/>
 					</ToolbarGroup>
 				</BlockControls>
 				<Fragment>
 					<RichTextShortcut
-						type="primary"
-						character={ character }
-						onUse={ onToggle }
+						type='primary'
+						character={character}
+						onUse={onToggle}
 					/>
 				</Fragment>
-				{ specialCharsPopover }
+				{specialCharsPopover}
 			</Fragment>
 		);
 	},
-} );
+});
