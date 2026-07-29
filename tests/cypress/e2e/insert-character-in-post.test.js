b0486c72bbf756f035cce6ebde0d072aa3334add
Darin Kotter <darin.kotter@gmail.com>
2026-07-29 09:54:16 -0600

    More test changes to try and ensure posts are created properly on multiple WP versions

--- tests/cypress/e2e/insert-character-in-post.test.js
+++ tests/cypress/e2e/insert-character-in-post.test.js
@@ -9,36 +9,51 @@ describe( 'Insert character in post', () => {
 	} );
 
 	it( 'Admin can add character in post and save it', () => {
-		cy.createPost( {
-			title: 'Post with special characters',
-			postType: 'post',
+		cy.visit( '/wp-admin/post-new.php?post_type=post' );
+
+		// Wait until the editor entity config is loaded.
+		cy.window().should( ( win ) => {
+			expect(
+				win.wp?.data?.select( 'core/editor' ).getCurrentPostType()
+			).to.eq( 'post' );
 		} );
 
+		cy.closeWelcomeGuide();
+
+		/**
+		 * Set title and insert a paragraph via the data stores so we do not
+		 * depend on getBlockEditor() / the iframed canvas for setup.
+		 */
 		cy.window().then( ( win ) => {
 			const { wp } = win;
+
+			wp.data.dispatch( 'core/editor' ).editPost( {
+				title: 'Post with special characters',
+			} );
 
 			const paraBlock = wp.blocks.createBlock( 'core/paragraph', {
 				content: 'Hello world',
 			} );
 
-			wp.data.dispatch( 'core/block-editor' ).insertBlocks( paraBlock );
-
-			return paraBlock.clientId;
-		} ).then( ( clientId ) => {
-			/**
-			 * Select the inserted paragraph in the editor canvas.
-			 * getBlockEditor() handles the iframed canvas in modern WP.
-			 */
-			cy.getBlockEditor().find( `#block-${ clientId }` ).click();
+			wp.data
+				.dispatch( 'core/block-editor' )
+				.insertBlocks( paraBlock );
+			wp.data
+				.dispatch( 'core/block-editor' )
+				.selectBlock( paraBlock.clientId );
 		} );
 
 		cy.get( '.toolbar-button__advanced-insertspecialcharacters' ).click();
 		cy.get( '.charMap--category button[data-title="FOR ALL"]' ).click( {
 			force: true,
 		} );
-		cy.get( '.editor-post-publish-button' ).click();
 
-		cy.wait( 1000 );
+		cy.get( '.editor-post-publish-panel__toggle' ).should( 'be.enabled' );
+		cy.get( '.editor-post-publish-panel__toggle' ).click();
+		cy.get( '.editor-post-publish-button' ).click();
+		cy.get( '.components-snackbar, .components-notice.is-success' ).should(
+			'be.visible'
+		);
 	} );
 
 	it( 'Verify the character on the front end', () => {
